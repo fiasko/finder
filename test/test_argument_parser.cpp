c9b16c927dd1960a63767668802fc4f51e597e54
Asko Eronen <asko.eronen@gmail.com>
2024-05-29 18:38:28 +0300

    add argument parser test to use assert

--- test/test_argument_parser.cpp
+++ test/test_argument_parser.cpp
@@ -13,51 +13,43 @@ TEST(ArgumentParser, NoArguments) {
 TEST(ArgumentParser, BasicArguments) {
   const char* argv[] = { "finder.exe", "X", "*.*", NULL };
   auto st = Finder::ParseArguments(3, argv);
-  EXPECT_TRUE(st.has_value());
-  if (st.has_value()) {
-    const auto& v = st.value();
-    EXPECT_FALSE(v.recursive_search);
-    EXPECT_FALSE(v.use_reg_exp);
-    EXPECT_STREQ(v.search_pattern.c_str(), "X");
-    EXPECT_STREQ(v.search_path.string().c_str(), "*.*");
-  }
+  ASSERT_TRUE(st.has_value());
+  const auto& v = st.value();
+  EXPECT_FALSE(v.recursive_search);
+  EXPECT_FALSE(v.use_reg_exp);
+  EXPECT_STREQ(v.search_pattern.c_str(), "X");
+  EXPECT_STREQ(v.search_path.string().c_str(), "*.*");
 }
 
 TEST(ArgumentParser, RegExpEnabled) {
   const char* argv[] = { "finder.exe", "-r", "AAAA", "testi\\", NULL};
   auto st = Finder::ParseArguments(4, argv);
-  EXPECT_TRUE(st.has_value());
-  if (st.has_value()) {
-    const auto& v = st.value();
-    EXPECT_TRUE(v.recursive_search);
-    EXPECT_FALSE(v.use_reg_exp);
-    EXPECT_STREQ(v.search_pattern.c_str(), "AAAA");
-    EXPECT_STREQ(v.search_path.string().c_str(), "testi\\");
-  }
+  ASSERT_TRUE(st.has_value());
+  const auto& v = st.value();
+  EXPECT_TRUE(v.recursive_search);
+  EXPECT_FALSE(v.use_reg_exp);
+  EXPECT_STREQ(v.search_pattern.c_str(), "AAAA");
+  EXPECT_STREQ(v.search_path.string().c_str(), "testi\\");
 }
 
 TEST(ArgumentParser, RegExpAndRecursiveEnabled) {
   const char* argv[] = { "finder.exe", "-r", "-G", "AAAA", "testi\\", NULL };
   auto st = Finder::ParseArguments(5, argv);
-  EXPECT_TRUE(st.has_value());
-  if (st.has_value()) {
-    const auto& v = st.value();
-    EXPECT_TRUE(v.recursive_search);
-    EXPECT_TRUE(v.use_reg_exp);
-    EXPECT_STREQ(v.search_pattern.c_str(), "AAAA");
-    EXPECT_STREQ(v.search_path.string().c_str(), "testi\\");
-  }
+  ASSERT_TRUE(st.has_value());
+  const auto& v = st.value();
+  EXPECT_TRUE(v.recursive_search);
+  EXPECT_TRUE(v.use_reg_exp);
+  EXPECT_STREQ(v.search_pattern.c_str(), "AAAA");
+  EXPECT_STREQ(v.search_path.string().c_str(), "testi\\");
 }
 
 TEST(ArgumentParser, RecursiveEnabled) {
   const char* argv[] = { "finder.exe", "-G", "AAAA", "testi\\", NULL };
   auto st = Finder::ParseArguments(4, argv);
-  EXPECT_TRUE(st.has_value());
-  if (st.has_value()) {
-    const auto& v = st.value();
-    EXPECT_FALSE(v.recursive_search);
-    EXPECT_TRUE(v.use_reg_exp);
-    EXPECT_STREQ(v.search_pattern.c_str(), "AAAA");
-    EXPECT_STREQ(v.search_path.string().c_str(), "testi\\");
-  }
+  ASSERT_TRUE(st.has_value());
+  const auto& v = st.value();
+  EXPECT_FALSE(v.recursive_search);
+  EXPECT_TRUE(v.use_reg_exp);
+  EXPECT_STREQ(v.search_pattern.c_str(), "AAAA");
+  EXPECT_STREQ(v.search_path.string().c_str(), "testi\\");
 }
